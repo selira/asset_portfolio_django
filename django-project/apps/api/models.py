7aa6338f5aaf849bccb3a0de6b8381b55813852b
selira <slira95@gmail.com>
2025-04-04 21:22:50 -0300

    models and migrations

--- django-project/apps/api/models.py
+++ django-project/apps/api/models.py
@@ -13,3 +13,32 @@ class Item(BaseModel):
     
     def __str__(self):
         return self.name
+    
+class Portfolio(BaseModel):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+    
+class PortfolioAssetWeight(BaseModel):
+    asset = models.ForeignKey('Asset', on_delete=models.CASCADE)
+    portfolio = models.ForeignKey('Portfolio', on_delete=models.CASCADE)
+    weight = models.DecimalField(max_digits=5, decimal_places=3)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.asset} - {self.weight}%"
+    
+class Asset(BaseModel):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+class AssetPrice(BaseModel):
+    asset = models.ForeignKey('Asset', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=8, decimal_places=5)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.asset} - {self.price}"
