ff8987e093331b4c78c40b1169dabe2e33caf615
selira <slira95@gmail.com>
2025-04-05 21:13:57 -0300

    portfolio history api working

--- django-project/apps/api/selectors.py
+++ django-project/apps/api/selectors.py
@@ -11,29 +11,6 @@ def item_list() -> List[dict]:
         'description': item.description
     } for item in items]
 
-def get_initial_quantities2(portfolio_id) -> List[dict]:
-    """
-    Get the initial quantities of each asset in the portfolio.
-    """
-    initial_date = '2022-02-15'
-    initial_portfolio_value = Decimal('1000000000')
-    initial_quantities = []
-    portfolio = Portfolio.objects.get(id=portfolio_id)
-    prices = AssetPrice.objects.filter(date=initial_date)
-    
-    weights = PortfolioAssetWeight.objects.filter(portfolio=portfolio, date=initial_date)
-    for weight in weights:
-        asset = weight.asset
-        price = prices.filter(asset_id=asset.id).first()
-        if price:
-            initial_quantity = (initial_portfolio_value * weight.weight) / price.price
-            initial_quantities.append({
-                'quantity': initial_quantity.quantize(Decimal('0.001')) if initial_quantity > 0 else Decimal('0.000'),
-                'asset': asset.name,
-                'asset_id': asset.id,
-            })
-    return initial_quantities
-
 def get_initial_quantities(portfolio_id: int) -> List[dict]:
     """
     Get the initial quantities of each asset in the portfolio.
@@ -76,44 +53,6 @@ def get_initial_quantities(portfolio_id: int) -> List[dict]:
     
     return initial_quantities
 
-def get_portfolio_history2(initial_date, final_date, portfolio_id) -> List[dict]:
-    """
-    Get the weight and portfolio value history of each asset in the portfolio.
-    """
-    if not initial_date or not final_date:
-        return []
-    
-    quantities = get_initial_quantities(portfolio_id)
-    quantities = [q for q in quantities if q['quantity'] > 0] #skip 0 weight assets
-    weight_history = {}
-    portfolio_history = {}
-    #get AssetPrice by date range, including limits, and group by date.
-    prices = AssetPrice.objects.filter(date__range=[initial_date, final_date])
-    for date in prices.values('date').distinct():
-        amounts = []
-        weight_history[date['date']] = {}
-        date_prices = prices.filter(date=date['date'])
-        portfolio_value = Decimal('0.000')
-        for quantity in quantities:
-            asset = date_prices.filter(asset_id=quantity['asset_id']).first()
-            if asset:
-                amount = asset.price * quantity['quantity']
-                portfolio_value += amount
-                amounts.append({
-                    'asset': quantity['asset'],
-                    'amount': amount,
-                    'date': date['date']
-                })
-        portfolio_history[date['date']] = portfolio_value
-        for amount in amounts:
-            weight = amount['amount'] / portfolio_value
-            weight_history[date['date']][amount['asset']] = weight.quantize(Decimal('0.00001')) if weight > 0 else Decimal('0.000')
-
-    return {
-        'weights': weight_history,
-        'portfolio_value': portfolio_history
-    }
-
 def get_portfolio_history(initial_date: str, final_date: str, portfolio_id: int) -> Dict[str, Any]:
     """
     Get the weight and portfolio value history of each asset in the portfolio.
@@ -152,7 +91,8 @@ def get_portfolio_history(initial_date: str, final_date: str, portfolio_id: int)
     for date, date_prices in prices_by_date.items():
         portfolio_value = Decimal('0.000')
         amounts = []
-        weight_history[date] = {}
+        formatted_date = date.strftime('%Y-%m-%d') #for json output
+        weight_history[formatted_date] = {}
 
         # Calculate portfolio value
         for asset_id, price in date_prices.items():
@@ -166,12 +106,12 @@ def get_portfolio_history(initial_date: str, final_date: str, portfolio_id: int)
                 })
 
         # Store portfolio value
-        portfolio_history[date] = portfolio_value.quantize(Decimal('0.01'), rounding='ROUND_HALF_UP')
+        portfolio_history[formatted_date] = portfolio_value.quantize(Decimal('0.01'), rounding='ROUND_HALF_UP')
 
         # Calculate weights
         for amount in amounts:
             weight = amount['amount'] / portfolio_value if portfolio_value else Decimal('0.000')
-            weight_history[date][amount['asset']] = (
+            weight_history[formatted_date][amount['asset']] = (
                 weight.quantize(Decimal('0.000001')) # check consistency with graph height.
                 if weight > 0 
                 else Decimal('0.000')
--- django-project/apps/api/views.py
+++ django-project/apps/api/views.py
@@ -19,12 +19,14 @@ class ItemListApi(APIView):
     
 class PortfolioHistoryApi(APIView):
     def get(self, request):
-        initial_date = request.query_params.get('initial_date')
-        final_date = request.query_params.get('final_date')
+        initial_date = request.query_params.get('fecha_inicio')
+        final_date = request.query_params.get('fecha_fin')
         portfolio_id = request.query_params.get('portfolio_id')
+        print(request.query_params)
+        print(initial_date, final_date, portfolio_id)
         if not initial_date or not final_date or not portfolio_id:
             return Response({"error": "Initial_date, final_date and portfolio ID are required "}, status=status.HTTP_400_BAD_REQUEST)
-        history = get_portfolio_history(request)
+        history = get_portfolio_history(initial_date, final_date, portfolio_id)
         if not history:
             return Response({"error": "No data found for the given date range."}, status=status.HTTP_404_NOT_FOUND)
         return Response(history, status=status.HTTP_200_OK)
